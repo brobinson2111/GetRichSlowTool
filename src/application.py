130e74c3e31050e5e94e1b3fe76b890373be6811
Brandon Robinson (brobinson2111) <brandon.robinson2111@gmail.com>
2018-08-02 14:51:51 -0700

    Add and Load Security Configurations JSON

--- src/application.py
+++ src/application.py
@@ -1,8 +1,15 @@
+import json
+import os
 
 class Application(object):
 
     def __init__(self):
         print("Application has been initialized...")
+        self.__root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+        self.__security_configurations_path = self.__root_dir + '/configuration/security_configurations.json'
 
     def run(self):
         print('Running Application...')
+        with open(self.__security_configurations_path, "r") as security_configurations:
+            data = json.load(security_configurations)
+            print('This is the first entry in data: ' + json.dumps(data[0]))
